c4dffb0f7c847b0306d804bbb61fa245fc6d009a
Steve Song <steve.xi.song@gmail.com>
2017-10-24 00:33:23 -0700

    add new blog - dynamo decade

---
+++ src/main/python/com.ActivationIntelligence/whoIsSteve2.py
@@ -0,0 +1,138 @@
+import math
+import dateutil.parser
+import datetime
+import time
+import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+""" --- Helpers to build responses which match the structure of the necessary dialog actions --- """
+
+
+def get_slots(intent_request):
+    return intent_request['currentIntent']['slots']
+
+
+def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'ElicitSlot',
+            'intentName': intent_name,
+            'slots': slots,
+            'slotToElicit': slot_to_elicit,
+            'message': message
+        }
+    }
+
+
+def close(session_attributes, fulfillment_state, message):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message
+        }
+    }
+
+    return response
+
+
+def delegate(session_attributes, slots):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Delegate',
+            'slots': slots
+        }
+    }
+
+
+""" --- Helper Functions --- """
+
+
+def parse_int(n):
+    try:
+        return int(n)
+    except ValueError:
+        return float('nan')
+
+
+def build_validation_result(is_valid, violated_slot, message_content):
+    if message_content is None:
+        return {
+            "isValid": is_valid,
+            "violatedSlot": violated_slot,
+        }
+
+    return {
+        'isValid': is_valid,
+        'violatedSlot': violated_slot,
+        'message': {'contentType': 'PlainText', 'content': message_content}
+    }
+
+
+def isvalid_date(date):
+    try:
+        dateutil.parser.parse(date)
+        return True
+    except ValueError:
+        return False
+
+""" --- Functions that control the bot's behavior --- """
+
+def who_is_steve(intent_request):
+
+    steve_name = get_slots(intent_request)["SteveName"]
+
+    outputText = "Something"
+    if steve_name:
+        outputText = steve_name + " is a software engineer. His enthusiasm is in Big Data and AI." \
+                                     "He is now working in the Amazon Alexa Machine Learning Platform team. " \
+                                     "He obtained his Master's degree from UW-Madison and worked with Professor Barton Miller at the Paradyn Lab." \
+                                     "To learn more about Steve, you can visit www.linkedin.com/in/songxi"
+
+    # Order the flowers, and rely on the goodbye message of the bot to define the message to the end user.
+    # In a real bot, this would likely involve a call to a backend service.
+    return close(intent_request['sessionAttributes'],
+                 'Fulfilled',
+                 {'contentType': 'PlainText',
+                  'content': outputText})
+
+""" --- Intents --- """
+
+
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    logger.debug('dispatch userId={}, intentName={}'.format(intent_request['userId'], intent_request['currentIntent']['name']))
+
+    intent_name = intent_request['currentIntent']['name']
+
+    # Dispatch to your bot's intent handlers
+    if intent_name == 'WhoIsSteve2':
+        return who_is_steve(intent_request)
+
+    raise Exception('Intent with name ' + intent_name + ' not supported')
+
+
+""" --- Main handler --- """
+
+
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+    # By default, treat the user request as coming from the America/New_York time zone.
+    os.environ['TZ'] = 'America/New_York'
+    time.tzset()
+    logger.debug('event.bot.name={}'.format(event['bot']['name']))
+
+    return dispatch(event)
